32976caaa1e52698b96c2f10a1e19b263787532f
Xin Wang <dram.wang@gmail.com>
2017-11-02 18:03:15 +0800

    Add skeleton for parse-sam-file-ng

--- tools/main.tcl
+++ tools/main.tcl
@@ -2,6 +2,16 @@ proc parse-sam-file {source} {
     return [exec -ignorestderr python3 tools/sam/samparser.py $source]
 }
 
+proc parse-sam-file-ng {source} {
+    set contents {}
+
+    set channel [open $source]
+    while {gets $channel line} {
+	puts stderr $line
+    }
+    close $channel
+}
+
 clips load tools/generate.clp
 
 clips reset
