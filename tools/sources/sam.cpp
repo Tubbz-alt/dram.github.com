68e9aa60083a2e15cdcb3d735a25fac37f1b8ccd
Xin Wang <dram.wang@gmail.com>
2018-10-14 12:19:11 +0800

    Use low-level Python API

--- tools/sources/sam.cpp
+++ tools/sources/sam.cpp
@@ -6,32 +6,26 @@
 #include "sam.hpp"
 
 std::optional<std::string> sam_parse(std::string path) {
-  static PyObject *globals = nullptr;
+  static PyObject *parser = nullptr;
 
-  if (globals == nullptr) {
+  if (parser == nullptr) {
     Py_Initialize();
 
-    globals = PyDict_New();
-
-    PyDict_SetItemString(globals, "__builtins__", PyEval_GetBuiltins());
-
-    PyRun_String("import sys;"
-                 "sys.path.append('tools/sam');"
-                 "import samparser",
-                 Py_file_input, globals, nullptr);
+    PyObject_CallMethod(PySys_GetObject("path"), "append", "s", "tools/sam");
+    parser = PyObject_CallMethod(PyImport_ImportModule("samparser"),
+                                 "SamParser", nullptr);
   }
 
-  PyRun_String(
-      ("p = samparser.SamParser(); p.parse_file('" + path + "')").c_str(),
-      Py_file_input, globals, nullptr);
+  PyObject_CallMethod(parser, "parse_file", "s", path.c_str());
+  PyObject *xml = PyObject_CallMethod(
+      PyBytes_FromString(""), "join", "O",
+      PyObject_CallMethod(PyObject_GetAttrString(parser, "doc"),
+                          "serialize_xml", nullptr));
 
-  PyObject *obj = PyRun_String("b''.join(p.doc.serialize_xml())", Py_eval_input,
-                               globals, nullptr);
-
-  if (obj == nullptr) {
+  if (xml == nullptr) {
     PyErr_Print();
     return std::nullopt;
   } else {
-    return std::optional<std::string>{PyBytes_AsString(obj)};
+    return std::optional<std::string>{PyBytes_AsString(xml)};
   }
 }
